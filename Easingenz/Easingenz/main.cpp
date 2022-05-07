cec653120e68d8c27cc54c5e9b67152710702f39
thuong.nv <thuong.nv@gmail.com>
2022-05-07 10:57:56 +0700

    Init first commit

---
+++ Easingenz/Easingenz/main.cpp
@@ -0,0 +1,185 @@
+﻿
+#include <GLWindow.h>
+#include <GLEasing.h>
+
+
+bool bfirst = true;
+glm::mat4   matView;
+glm::mat4   matProject;
+float zoom = 1.0f;
+
+MenuContext* menucontext;
+
+void OpenFile(Window* win, Control* mn)
+{
+    MessageBox(NULL, "OpenFile", "Thong bao", MB_ICONWARNING);
+}
+
+void SaveFile(Window* win, Control* mn)
+{
+    MessageBox(NULL, "SaveFile", "Thong bao", MB_ICONWARNING);
+}
+
+void OnButton(Window* win, Control* btn)
+{
+    MessageBox(NULL, "ON button", "Thong bao", MB_ICONWARNING);
+}
+
+
+void Create(Window* win)
+{
+    matView = glm::lookAt(glm::vec3(0, 0, 8), glm::vec3(0, 0, 8) + glm::vec3(0, 0, -1), glm::vec3(0.f, 1.f, 0.f));
+
+    int width = win->GetWidth();
+    int height = win->GetHeight();
+
+    GLdouble left = -GLdouble(width / 2) / zoom;
+    GLdouble right = GLdouble(width / 2) / zoom;
+    GLdouble top = -GLdouble(height / 2) / zoom;
+    GLdouble bottom = GLdouble(height / 2) / zoom;
+    glViewport(0, 0, width, height);
+    matProject = glm::ortho(left, right, top, bottom, double(0.1), double(-1000));
+
+    MenuItemBase item;
+    MenuBar* menubar = new MenuBar();
+
+    MenuBarItem menubaritem("File");
+
+    item.SetLabel("Open file");
+    item.SetType(MF_STRING);
+    item.SetEvent(OpenFile);
+    menubaritem.AddItem(item);
+
+    item.SetType(MF_SEPARATOR);
+    menubaritem.AddItem(item);
+
+    item.SetLabel("Save file");
+    item.SetType(MF_STRING);
+    item.SetEvent(OpenFile);
+    menubaritem.AddItem(item);
+
+    menubar->AddItem(menubaritem);
+
+    menubaritem.SetText("Edit");
+
+    item.SetLabel("Create connect");
+    item.SetType(MF_STRING);
+    item.SetEvent(OpenFile);
+    menubaritem.AddItem(item);
+
+    item.SetType(MF_SEPARATOR);
+    menubaritem.AddItem(item);
+
+    item.SetLabel("Save file");
+    item.SetType(MF_STRING);
+    item.SetEvent(OpenFile);
+    menubaritem.AddItem(item);
+
+    menubar->AddItem(menubaritem);
+
+    win->AddControl(menubar);
+}
+
+void MouseButton(Window* win)
+{
+    if (win->GetMouseButtonStatus(GLMouse::LeftButton))
+    {
+        zoom += 0.2f;
+
+        matView = glm::lookAt(glm::vec3(0, 0, 8), glm::vec3(0, 0, 8) + glm::vec3(0, 0, -1), glm::vec3(0.f, 1.f, 0.f));
+
+        int width = win->GetWidth();
+        int height = win->GetHeight();
+
+        GLdouble left = -GLdouble(width / 2) / zoom;
+        GLdouble right = GLdouble(width / 2) / zoom;
+        GLdouble top  = -GLdouble(height / 2) / zoom;
+        GLdouble bottom = GLdouble(height / 2) / zoom;
+        glViewport(0, 0, width, height);
+        matProject = glm::ortho(left, right, top, bottom, double(0.1), double(-1000));
+    }
+}
+
+void Keyboard(Window* win)
+{
+    if (win->GetKeyboardStatus(GLKeyboard::Escapex))
+    {
+        win->ExitFullScreen();
+    }
+
+    else if (win->GetKeyboardStatus(GLKeyboard::KeyA))
+    {
+        menucontext->Show(POINT{ 0 ,0 });
+    }
+}
+
+
+void Draw(Window* win)
+{
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadMatrixf(glm::value_ptr(matProject));
+    glMatrixMode(GL_MODELVIEW);
+    glLoadMatrixf(glm::value_ptr(matView));
+
+    glBegin(GL_TRIANGLES);
+
+    glColor3f(0.5, 1.0, 0);
+
+    glVertex3f(100.0, 100.0, 100.0);
+    glVertex3f(0.0, 100.0, 100.0);
+    glVertex3f(100.0, 0.0, 100.0);
+
+    glEnd();
+}
+
+void Resize(Window* win)
+{
+    int width = win->GetWidth();
+    int height = win->GetHeight();
+    GLdouble left = -GLdouble(width / 2) / zoom;
+    GLdouble right = GLdouble(width / 2) / zoom;
+    GLdouble top = -GLdouble(height / 2) / zoom;
+    GLdouble bottom = GLdouble(height / 2) / zoom;
+    matProject = glm::ortho(left, right, top, bottom, double(0.1), double(-1000));
+}
+
+void Process(Window * pWin)
+{
+}
+
+//int main()
+//{
+//    GLWindow window;
+//
+//    Window *win1 = new Window("Easingenz", 100, 100);
+//    win1->SetOnDrawfunc(Draw);
+//    win1->SetOnCreatedfunc(Create);
+//    win1->SetOnMouseButtonfunc(MouseButton);
+//    win1->SetProcessfunc(Process);
+//    win1->SetOnKeyboardfunc(Keyboard);
+//    win1->SetOnResizefunc(Resize);
+//
+//    window.AddWindow(win1);
+//
+//    while (window.Closed())
+//    {
+//        window.Process();
+//
+//        window.Draw();
+//        window.PollEvent();
+//    }
+//}
+
+int main()
+{
+    for (float i = 0; i < 1; i+=0.01)
+    {
+        float o = EaseOutBack(i);
+        printf("%f\n", o);
+    }
+
+    getchar();
+}
