581d44a983999678b23473ab28d71cff1f0bbefd
thuong.nv <thuong.nv@gmail.com>
2022-05-14 11:02:43 +0700

    [:)] UI + test ok => OK

--- Easingenz/Easingenz/main.cpp
+++ Easingenz/Easingenz/main.cpp
@@ -1,10 +1,7 @@
-﻿
-#include <GLWindow.h>
+﻿#include <GLWindow.h>
 #include <GLEasing.h>
 #include <GLSp.h>
 
-
-bool bfirst = true;
 glm::mat4   matView;
 glm::mat4   matProject;
 float zoom = 1.0f;
@@ -13,11 +10,16 @@ float startpox = -300.f;
 float endpox   =  300.f;
 
 
-EasingBounce animationBack;
+EasingBack    animationBack;
+EasingBounce  animationBounce;
+EasingElastic animationElastic;
+EasingQuart   animationQuart;
+EasingQuint   animationQuint;
 
 EasingBase* animation;
 
-Combobox* cbb;
+Combobox* cbbType;
+Combobox* cbbMode;
 
 
 bool bCheck = false;
@@ -27,8 +29,33 @@ MenuContext* menucontext;
 
 void btnclick(Window* win, Button* mn)
 {
-    animationBack.Start();
-    cout << cbb->GetSelectIndex() << endl;
+    EaseType type = static_cast<EaseType>(*(int*)cbbType->GetSelectData());
+    EaseMode mode = static_cast<EaseMode>(*(int*)cbbMode->GetSelectData());
+
+    cout << "Type :" << cbbType->GetSelectText() << " + " << cbbMode->GetSelectText() << endl;
+
+    switch (type)
+    {
+    case EaseType::Back:
+        animation = &animationBack;
+        break;
+    case EaseType::Quint:
+        animation = &animationQuint;
+        break;
+    case EaseType::Elastic:
+        animation = &animationElastic;
+        break;
+    case EaseType::Quart:
+        animation = &animationQuart;
+        break;
+    case EaseType::Bounce:
+        animation = &animationBounce;
+        break;
+    default:
+        break;
+    }
+    animation->SetMode(mode);
+    animation->Start();
 }
 
 
@@ -47,26 +74,43 @@ void Create(Window* win)
     matProject = glm::ortho(left, right, top, bottom, double(0.1), double(-1000));
 
     Button* btn = new Button();
-    btn->SetPosition(0, 0);
+    btn->SetPosition(250, 0);
     btn->SetEvent(btnclick);
-    btn->SetSize(100, 50);
-    btn->SetLabel("Click");
+    btn->SetSize(50, 25);
+    btn->SetLabel("Run");
 
 
-    cbb = new Combobox(200, 0, 200, 300);
-    cbb->TextEdit(false);
+    cbbType = new Combobox(0, 0, 120, 300);
+    cbbType->TextEdit(false);
 
-    cbb->AddItem("gia tri 1");
-    cbb->AddItem("gia tri 2");
-    cbb->AddItem("gia tri 3");
+    cbbType->AddItem("Back"   , (void*) new int((int)EaseType::Back));
+    cbbType->AddItem("Bounce" , (void*) new int((int)EaseType::Bounce));
 
-    cbb->SetSelect(4);
+    cbbType->AddItem("Elastic", (void*) new int((int)EaseType::Elastic));
+    cbbType->AddItem("Quart"  , (void*) new int((int)EaseType::Quart));
+    cbbType->AddItem("Quint"  , (void*) new int((int)EaseType::Quint));
+    cbbType->SetSelect(0);
+
+
+    cbbMode = new Combobox(125, 0, 120, 300);
+    cbbMode->TextEdit(false);
+    cbbMode->AddItem("In"   , (void*) new int((int)EaseMode::In));
+    cbbMode->AddItem("Out"  , (void*) new int((int)EaseMode::Out));
+    cbbMode->AddItem("InOut", (void*) new int((int)EaseMode::InOut));
+
+    cbbMode->SetSelect(0);
 
     animationBack.Setup(EaseMode::Out, startpox, endpox, 2.f);
+    animationBounce.Setup(EaseMode::Out, startpox, endpox, 2.f);
+    animationElastic.Setup(EaseMode::Out, startpox, endpox, 2.f);
+    animationQuart.Setup(EaseMode::Out, startpox, endpox, 2.f);
+    animationQuint.Setup(EaseMode::Out, startpox, endpox, 2.f);
+
     animation = &animationBack;
 
     win->AddControl(btn);
-    win->AddControl(cbb);
+    win->AddControl(cbbType);
+    win->AddControl(cbbMode);
 }
 
 void MouseButton(Window* win)
@@ -77,7 +121,7 @@ void MouseButton(Window* win)
 
         matView = glm::lookAt(glm::vec3(0, 0, 8), glm::vec3(0, 0, 8) + glm::vec3(0, 0, -1), glm::vec3(0.f, 1.f, 0.f));
 
-        int width = win->GetWidth();
+        int width  = win->GetWidth();
         int height = win->GetHeight();
 
         GLdouble left = -GLdouble(width / 2) / zoom;
@@ -102,7 +146,6 @@ void Keyboard(Window* win)
     }
 }
 
-
 void Draw(Window* win)
 {
     glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
@@ -118,12 +161,12 @@ void Draw(Window* win)
 
 void Resize(Window* win)
 {
-    int width = win->GetWidth();
+    int width  = win->GetWidth();
     int height = win->GetHeight();
-    GLdouble left = -GLdouble(width / 2) / zoom;
-    GLdouble right = GLdouble(width / 2) / zoom;
-    GLdouble top = -GLdouble(height / 2) / zoom;
-    GLdouble bottom = GLdouble(height / 2) / zoom;
+    GLdouble left   = -GLdouble(width / 2) / zoom;
+    GLdouble right  =  GLdouble(width / 2) / zoom;
+    GLdouble top    = -GLdouble(height / 2) / zoom;
+    GLdouble bottom =  GLdouble(height / 2) / zoom;
     matProject = glm::ortho(left, right, top, bottom, double(0.1), double(-1000));
 }
 
@@ -160,14 +203,3 @@ int main()
         window.PollEvent();
     }
 }
-
-//int main()
-//{
-//    for (float i = 0; i < 1; i+=0.01)
-//    {
-//        float o = EaseOutBack(i);
-//        printf("%f\n", o);
-//    }
-//
-//    getchar();
-//}
